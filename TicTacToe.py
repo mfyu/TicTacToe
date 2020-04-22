c5019c21af0a7adec97d5c1726492b055085ef7f
mfyu <myu121@gmail.com>
2020-04-22 02:26:37 -0700

    Networking

---
+++ TicTacToe.py
@@ -0,0 +1,158 @@
+from sys import exit
+from os import environ
+import pygame, math, sys
+
+SCREENSIZE = (300, 370)
+
+environ['SDL_VIDEO_CENTERED'] = '1'
+pygame.init()
+screen = pygame.display.set_mode(SCREENSIZE)
+white = (255,255,255)
+black = (0,0,0)
+pygame.font.init()
+fnt = pygame.font.SysFont("Arial", 18)
+txtpos = (100, 90)
+
+
+class TicTacToe:
+    def __init__(self):
+        self.statusLabel = "connecting"
+        self.playersLabel = "0 players"
+        self.turn = 1
+        self.board = [[None, None, None],[None, None, None],[None, None, None]]
+        self.gameOver = False
+        self.draw = False
+        self.drawNewBoard()
+        
+    
+    def Events(self):
+        for event in pygame.event.get():
+            pygame.display.update()
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == 27):
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.gameOver is False:
+                    self.Click(event)
+                else:
+                    self.drawNewBoard()
+                    self.newGame()
+                    
+    
+    def Turn(self, data):
+
+        print("turn")
+        row = data['row']
+        col = data['col']
+        xo = "X"
+        #screen.fill([0, 0, 0])
+        white = (255,255,250)
+
+        x = col*100+50
+        y = row*100+50
+        #check if board position is empty
+        if (row<3 and col<3):
+            if (self.board[row][col] is None):
+                if (self.turn == 2):
+                    pygame.draw.circle(screen, white, (x,y), 30,2)
+                    self.board[row][col] = 'X'
+                    self.turn = 1
+                    print(self.turn)
+                elif(self.turn == 1):
+                    pygame.draw.line(screen, white, (x-25,y-25), (x+25,y+25), 2)
+                    pygame.draw.line(screen, white, (x+25,y-25), (x-25,y+25), 2)
+                    self.board[row][col] = 'O'
+                    self.turn = 2
+                    print(self.turn)
+
+            self.checkWin()
+            if self.gameOver:
+                pygame.draw.rect(screen, black, [0, 310, 300, 330])
+                if (self.draw):
+                	txt = "Draw" 
+                elif (self.turn == 2):
+                	txt = "X Wins!"
+                else:
+                    txt = "O Wins!"
+                text = fnt.render(txt, True, (0,250,0))
+                screen.blit(text, (90,310))
+                text = fnt.render("Click to play again", True, (0,250,0))
+                screen.blit(text, (80,335))
+                pygame.display.update()
+            else:
+                pygame.draw.rect(screen, black, [0, 310, 300, 330])
+                if (self.turn == 1):
+                    txt = "X's Turn"
+                else:
+                    txt = "O's Turn"
+                
+                text = fnt.render(txt, True, white)
+                screen.blit(text, (90,310))
+                pygame.display.update()
+
+    def checkWin(self):
+    
+        for row in range (0,3):
+            #row win
+            if ((self.board[row][0] == self.board[row][1] == self.board[row][2]) and self.board[row][0] is not None):
+                pygame.draw.line(screen, (0,250,0), (0, row*100+50), (300, row*100+50), 2)
+                if (self.board[row][0]=='X'):
+                    print("X win")
+                    #p1win(screen)
+                else:
+                    print("O win")
+                    #p2win(screen)
+                self.gameOver = True
+        for col in range (0,3):
+            #col win
+            if ((self.board[0][col] == self.board[1][col] == self.board[2][col]) and self.board[0][col] is not None):
+                pygame.draw.line(screen, (0,250,0), (col*100+50,0), (col*100+50,300), 2)
+                if (self.board[0][col]=='X'):
+                    print("X win")
+                    #p1win(screen)
+                else:
+                    print("O win")
+                    #p2win(screen)
+                self.gameOver = True
+
+        #diagonal wins
+        if ((self.board[0][0] == self.board[1][1] == self.board[2][2]) and self.board[0][0] is not None):
+            pygame.draw.line(screen, (0,250,0), (0,0), (300,300), 2)
+            if (self.board[0][0]=='X'):
+                print("X win")
+                #p1win(screen)
+            else:
+                print("O win")
+                #p2win(screen)
+            self.gameOver = True
+        if ((self.board[2][0] == self.board[1][1] == self.board[0][2]) and self.board[2][0] is not None):
+            pygame.draw.line(screen, (0,250,0), (0,300), (300,0), 2)
+            if (self.board[2][0]=='X'):
+                print("X win")
+                #p1win(screen)
+            else:
+                print("O win")
+                #p2win(screen)
+            self.gameOver = True
+        #draw
+        if (self.board[0][0] is not None and self.board[0][1] is not None and self.board[0][2] is not None and 
+            self.board[1][0] is not None and self.board[1][1] is not None and self.board[1][2] is not None and 
+            self.board[2][0] is not None and self.board[2][1] is not None and self.board[2][2]):
+            print("draw")
+            self.draw = True
+            #draw(screen)
+            self.gameOver = True
+
+    def drawNewBoard(self):
+        self.turn = 1
+        self.gameOver = False
+        self.board = [[None, None, None],
+                 [None, None, None],
+                 [None, None, None]]
+        screen.fill(black)
+        pygame.draw.line(screen, (250,250,250), (100, 0), (100, 300), 2)
+        pygame.draw.line(screen, (250,250,250), (200, 0), (200, 300), 2)
+        pygame.draw.line(screen, (250,250,250), (0, 100), (300, 100), 2)
+        pygame.draw.line(screen, (250,250,250), (0, 200), (300, 200), 2)
+        text = fnt.render("X's Turn", True, white)
+        screen.blit(text, (90,310))
+        pygame.display.update()
---
+++ client.py
@@ -0,0 +1,70 @@
+import sys, math
+from time import sleep
+from PodSixNet.Connection import connection, ConnectionListener
+from TicTacToe import TicTacToe
+
+class Client(ConnectionListener, TicTacToe):
+    def __init__(self, host, port):
+        self.Connect((host, port))
+        self.players = {}
+        self.data = []
+        TicTacToe.__init__(self)
+    
+    def Loop(self):
+        self.Pump()
+        connection.Pump()
+        self.Events()
+        
+
+    def Click(self, e):
+        print(e.pos)
+        col = math.floor(e.pos[0]/100)
+        row = math.floor(e.pos[1]/100)
+        connection.Send({"action": "click", "position": e.pos, "turn" : "x", "row":row, "col": col})
+
+    def newGame(self):
+
+        connection.Send({"action":"newgame"})
+    
+    ###############################
+    ### Network event callbacks ###
+    ###############################
+    def Network_newgame(self,data):
+        print(data)
+        self.drawNewBoard()
+    def Network_click(self,data):
+        print("click")
+        print(data)
+        self.Turn(data)
+   
+    
+    def Network(self, data):
+        #print('network:', data)
+        pass
+    
+    def Network_connected(self, data):
+        self.statusLabel = "connected"
+        print("CONNECTED")
+        print(data)
+    
+    def Network_error(self, data):
+        print(data)
+        import traceback
+        traceback.print_exc()
+        self.statusLabel = data['error'][1]
+        connection.Close()
+    
+    def Network_disconnected(self, data):
+        self.statusLabel += " - disconnected"
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage:", sys.argv[0], "host:port")
+        print("e.g.", sys.argv[0], "localhost:31425")
+    else:
+        host, port = sys.argv[1].split(":")
+        c = Client(host, int(port))
+        #c = Client("localhost", 12345)
+        while 1:
+            c.Loop()
+            sleep(0.001)
